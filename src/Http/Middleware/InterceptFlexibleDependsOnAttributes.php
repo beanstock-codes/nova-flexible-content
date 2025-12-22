78f1d08fd5c43ef088f76fba99efe457f0dde16b
Peter Ragheb <peteragheb@gmail.com>
2025-12-22 14:24:08 +0200

    Fix InterceptFlexibleDependsOnAttributes logic to only update flexible field nested fields

--- src/Http/Middleware/InterceptFlexibleDependsOnAttributes.php
+++ src/Http/Middleware/InterceptFlexibleDependsOnAttributes.php
@@ -4,8 +4,22 @@ namespace Whitecube\NovaFlexibleContent\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Fields\FieldCollection;
+use Laravel\Nova\Http\Requests\ActionRequest;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Http\Requests\ResourceCreateOrAttachRequest;
+use Laravel\Nova\Http\Requests\ResourceUpdateOrUpdateAttachedRequest;
+use Laravel\Nova\Http\Resources\CreateViewResource;
+use Laravel\Nova\Http\Resources\CreationPivotFieldResource;
+use Laravel\Nova\Http\Resources\ReplicateViewResource;
+use Laravel\Nova\Http\Resources\UpdatePivotFieldResource;
+use Laravel\Nova\Http\Resources\UpdateViewResource;
 use Symfony\Component\HttpFoundation\Response;
+use Whitecube\NovaFlexibleContent\Flexible;
 use Whitecube\NovaFlexibleContent\Http\FlexibleAttribute;
+use Whitecube\NovaFlexibleContent\Layouts\LayoutInterface;
+use \Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class InterceptFlexibleDependsOnAttributes
 {
@@ -56,10 +70,8 @@ class InterceptFlexibleDependsOnAttributes
      */
     protected function isDependentFieldRequest(Request $request): bool
     {
-        $path = $request->path();
-
-        // Match Nova API routes for creation-fields, update-fields, and action
-        return preg_match('/nova-api\/[^\/]+(?:\/\d+)?\/(?:creation-fields|update-fields|action)/', $path) === 1;
+        return $request->isMethod(SymfonyRequest::METHOD_PATCH)
+            && $this->getNovaRequestInstance($request) !== null;
     }
 
     /**
@@ -70,20 +82,144 @@ class InterceptFlexibleDependsOnAttributes
      */
     protected function hasFlexibleFieldNames(Request $request): bool
     {
-        // Check if the 'field' parameter or any input contains the flexible group separator
-        $field = $request->get('field');
+        $fields = $this->getNovaRequestFields($request);
+        $jsonKeys = $request->json() ? $request->json()->keys() : [];
+        $inputFields = array_filter(array_merge($jsonKeys, [$request->get('field')]));
 
-        if ($field && strpos($field, FlexibleAttribute::GROUP_SEPARATOR) !== false) {
-            return true;
-        }
-
-        foreach ($request->all() as $key => $value) {
-            if (strpos($key, FlexibleAttribute::GROUP_SEPARATOR) !== false) {
+        foreach ($inputFields as $inputField) {
+            $matchedField = $this->findFieldByAttribute($fields, $inputField);
+            if ($matchedField instanceof Flexible) {
                 return true;
             }
         }
 
         return false;
+    }
+
+    /**
+     * Get the appropriate Nova request instance based on the request path.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Laravel\Nova\Http\Requests\NovaRequest|null
+     */
+    protected function getNovaRequestInstance(Request $request): ?NovaRequest
+    {
+        $path = $request->path();
+
+        $pattern = '/nova-api\/[^\/]+(?:\/\d+)?\/(creation-fields|update-fields|creation-pivot-fields\/[^\/]+|update-pivot-fields\/[^\/]+(?:\/\d+)?|action)/';
+        if (preg_match($pattern, $path, $matches) === 1) {
+            $route = $matches[1];
+            if (strpos($route, 'creation-fields') !== false || strpos($route, 'creation-pivot-fields') !== false) {
+                return app(ResourceCreateOrAttachRequest::class);
+            } elseif (strpos($route, 'update-fields') !== false || strpos($route, 'update-pivot-fields') !== false) {
+                return app(ResourceUpdateOrUpdateAttachedRequest::class);
+            } elseif ($route === 'action') {
+                return app(ActionRequest::class);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Check if the request is for a pivot resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function isPivotRequest(Request $request): bool
+    {
+        $path = $request->path();
+        return strpos($path, 'creation-pivot-fields') !== false
+            || strpos($path, 'update-pivot-fields') !== false;
+    }
+
+    protected function getNovaRequestFields(Request $request): array
+    {
+        $novaRequest = $this->getNovaRequestInstance($request);
+
+        if (is_null($novaRequest)) {
+            return [];
+        }
+
+        $class = get_class($novaRequest);
+
+        switch ($class) {
+            case ResourceCreateOrAttachRequest::class:
+                return $this->getCreateRequestFields($novaRequest);
+            case ResourceUpdateOrUpdateAttachedRequest::class:
+                return $this->getUpdateRequestFields($novaRequest);
+            case ActionRequest::class:
+                return $this->getActionRequestFields($novaRequest);
+            default:
+                return [];
+        }
+    }
+
+    protected function getCreateRequestFields(ResourceCreateOrAttachRequest $request): array
+    {
+        // Check if this is a pivot resource request
+        if ($this->isPivotRequest($request)) {
+            $resource = CreationPivotFieldResource::make()->newResourceWith($request);
+            return $resource->creationPivotFields($request, $request->relatedResource)->all();
+        }
+
+        // Regular resource request
+        $resource = $request->has('fromResourceId')
+            ? ReplicateViewResource::make($request->fromResourceId)->newResourceWith($request)
+            : CreateViewResource::make()->newResourceWith($request);
+
+        return $resource->creationFields($request)->all();
+    }
+
+    protected function getUpdateRequestFields(ResourceUpdateOrUpdateAttachedRequest $request): array
+    {
+        // Check if this is a pivot resource request
+        if ($this->isPivotRequest($request)) {
+            $resource = UpdatePivotFieldResource::make()->newResourceWith($request);
+            return $resource->updatePivotFields($request, $request->relatedResource)->all();
+        }
+
+        // Regular resource request
+        $resource = UpdateViewResource::make()->newResourceWith($request);
+        return $resource->updateFields($request)->all();
+    }
+
+    protected function getActionRequestFields(ActionRequest $request): array
+    {
+        return $request->action()->fields($request);
+
+    }
+
+    public function findFieldByAttribute(array $fields, string $attribute): ?Field
+    {
+        $matchedField = FieldCollection::make($fields)->findFieldByAttribute($attribute);
+        if (!is_null($matchedField)) {
+            return $matchedField;
+        }
+
+        foreach ($fields as $field) {
+            if ($field instanceof Flexible) {
+                $closure = function () {
+                    return $this->layouts;
+                };
+                $layouts = $closure->call($field);
+                /** @var LayoutInterface $layout */
+                foreach ($layouts as $layout) {
+                    $matchedField = $this->findFieldByAttribute($layout->fields(), $attribute);
+                    if (!is_null($matchedField)) {
+                        return $matchedField;
+                    }
+                }
+            } elseif (method_exists($field, 'getFields')) {
+                $matchedField = $this->findFieldByAttribute($field->getFields(), $attribute);
+                if (!is_null($matchedField)) {
+                    return $matchedField;
+                }
+            }
+        }
+
+        return null;
     }
 
     /**
@@ -95,15 +231,20 @@ class InterceptFlexibleDependsOnAttributes
     protected function transformFlexibleFieldNames(Request $request): void
     {
         // Transform the 'field' query parameter if present
-        $field = $request->get('field');
+        $fieldName = $request->get('field');
+        $newFieldName = $this->stripGroupPrefix($fieldName);
 
-        if ($field && strpos($field, FlexibleAttribute::GROUP_SEPARATOR) !== false) {
-            $transformedField = $this->stripGroupPrefix($field);
-            $request->query->set('field', $transformedField);
+        $fields = $this->getNovaRequestFields($request);
+
+        $matchedField = $this->findFieldByAttribute($fields, $newFieldName);
+
+        if (! is_null($fieldName) && ! is_null($matchedField)
+            && strpos($fieldName, FlexibleAttribute::GROUP_SEPARATOR) !== false) {
+            $request->query->set('field', $newFieldName);
         }
 
         // Transform all input field names
-        $transformedInput = $this->transformInputFields($request->all());
+        $transformedInput = $this->transformInputFields($fields, $request->all());
 
         if (! empty($transformedInput)) {
             $request->merge($transformedInput);
@@ -116,13 +257,17 @@ class InterceptFlexibleDependsOnAttributes
      * @param  array  $input
      * @return array
      */
-    protected function transformInputFields(array $input): array
+    protected function transformInputFields(array $fields , array $input): array
     {
         $transformed = [];
 
         foreach ($input as $key => $value) {
-            if (strpos($key, FlexibleAttribute::GROUP_SEPARATOR) !== false) {
-                $newKey = $this->stripGroupPrefix($key);
+            if (strpos($key, FlexibleAttribute::GROUP_SEPARATOR) === false) {
+                continue;
+            }
+
+            $newKey = $this->stripGroupPrefix($key);
+            if ($this->findFieldByAttribute($fields, $newKey)) {
                 $transformed[$newKey] = $value;
             }
         }
@@ -132,18 +277,23 @@ class InterceptFlexibleDependsOnAttributes
 
     /**
      * Strip the flexible group prefix from a field name.
+     * Only strips if the field name matches the flexible field pattern: c[A-Za-z0-9]{15}__field_name
      *
      * @param  string  $fieldName
      * @return string
      */
     protected function stripGroupPrefix(string $fieldName): string
     {
-        $separatorPosition = strpos($fieldName, FlexibleAttribute::GROUP_SEPARATOR);
+        // Validate that this matches the flexible field naming pattern
+        $separator = preg_quote(FlexibleAttribute::GROUP_SEPARATOR, '/');
+        $pattern = '/^c[A-Za-z0-9]{15}' . $separator . '(.+)$/';
 
-        if ($separatorPosition === false) {
-            return $fieldName;
+        if (preg_match($pattern, $fieldName, $matches) === 1) {
+            // Return just the field name without the group prefix
+            return $matches[1];
         }
 
-        return substr($fieldName, $separatorPosition + strlen(FlexibleAttribute::GROUP_SEPARATOR));
+        // Not a flexible field pattern, return as-is
+        return $fieldName;
     }
 }
