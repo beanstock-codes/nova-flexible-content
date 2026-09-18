58aba74b782ce6c57213cb925de85c9960f41352
Peter Ragheb <peteragheb@gmail.com>
2026-09-19 00:10:28 +0300

    Use laravel-nova-ui icons and build against Nova 5

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,25 @@
 let mix = require('laravel-mix')
 let path = require('path')
-let NovaExtension = require('laravel-nova-devtool')
 
-mix.extend('nova', new NovaExtension())
+const novaPath = process.env.NOVA_PATH || path.join(__dirname, 'vendor/laravel/nova')
 
 mix
     .setPublicPath('dist')
     .js('resources/js/field.js', 'js')
     .vue({ version: 3 })
     .sass('resources/sass/field.scss', 'css')
-    .nova('whitecube/nova-flexible-content');
+    .webpackConfig({
+        externals: {
+            vue: 'Vue',
+            'laravel-nova-ui': 'LaravelNovaUi',
+        },
+        resolve: {
+            alias: {
+                'laravel-nova': path.join(novaPath, 'resources/js/mixins/packages.js'),
+                '@': path.join(novaPath, 'resources/js'),
+            },
+        },
+        output: {
+            uniqueName: 'whitecube/nova-flexible-content',
+        },
+    })
